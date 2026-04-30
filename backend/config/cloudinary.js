9f206c125e6f16ea057cc55c817416173b2e91ca
elfaradio <farhadulislam84054@gmail.com>
2026-04-30 23:03:03 +0600

    update

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -5,24 +5,24 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// Configure Cloudinary with your credentials
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-// Set up the storage engine
+
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
-    folder: "rentx_flats", // This creates a specific folder inside your Cloudinary account
+    folder: "rentx_flats",
     allowed_formats: ["jpg", "jpeg", "png", "webp"],
-    transformation: [{ width: 1000, height: 1000, crop: "limit" }], // Optional: prevents massive file uploads
+    transformation: [{ width: 1000, height: 1000, crop: "limit" }],
   },
 });
 
-// Initialize Multer with this storage engine
+
 const upload = multer({ storage });
 
 export { cloudinary, upload };
