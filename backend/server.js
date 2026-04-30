76731459e4346a01611c8e243c31bfac9b29e2a6
elfaradio <farhadulislam84054@gmail.com>
2026-04-30 23:03:55 +0600

    update

--- backend/server.js
+++ backend/server.js
@@ -5,29 +5,26 @@ import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import flatRoutes from "./routes/flatRoutes.js";
 
-// Load environment variables
 dotenv.config();
 
-// Connect to the database
+
 connectDB();
 
-// Initialize Express app
 const app = express();
 
-// Middleware to parse JSON bodies and allow cross-origin requests
 app.use(express.json());
 app.use(cors());
 
-// Basic test route
+
 app.get("/", (req, res) => {
   res.send("Rentx API is running...");
 });
 
-// Mount the API routes
+
 app.use("/api/users", userRoutes);
 app.use("/api/flats", flatRoutes);
 
-// Start the server
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
